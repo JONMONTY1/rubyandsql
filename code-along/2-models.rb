2bc9d614a3de39c8969977625efd5ab036a4681c
Jonathan Monty <jonathan.monty@kellogg.northwestern.edu>
2023-05-07 05:44:07 +0000

    associations

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -14,14 +14,61 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+# puts "There are #{Company.all.count} companies."
+
 # 2. insert new rows in companies table
+
+new_company = Company.new
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "CA"
+new_company["url"] = "https://www.apple.com"
+new_company.save
+
+new_company2 = Company.new
+new_company2["name"] = "Amazon"
+new_company2["city"] = "Seattle"
+new_company2["state"] = "WA"
+new_company2.save
+
+new_company3 = Company.new
+new_company3["name"] = "Twitter"
+new_company3["city"] = "San Francisco"
+new_company3["state"] = "CA"
+new_company3.save
+
+puts "companies: #{Company.all.count}"
 
 # 3. query companies table to find all row with California company
 
+all_companies = Company.all
+puts all_companies.inspect
+
+california_companies = Company.where({ "state" => "CA" })
+puts california_companies.inspect
+puts "companies in cali: #{california_companies.count}"
+
 # 4. query companies table to find single row for Apple
+
+apple = Company.find_by({ "name" => "Apple" })
+puts apple.inspect
+
+puts apple["url"]
 
 # 5. read a row's column value
 
+puts apple["url"]
+
 # 6. update a row's column value
 
+amazon = Company.find_by({ "name" => "Amazon" })
+amazon["url"] = "https://www.amazon.com/"
+# OR
+# amazon.url = "https://www.amazon.com/"
+amazon.save
+
 # 7. delete a row
+
+twitter = Company.find_by({ "name" => "Twitter" })
+twitter.destroy
+
